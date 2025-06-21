9f7e0235491617c2085e78ef82912f1530ef19f3
Manoj Nath <161053926+manojnath18@users.noreply.github.com>
2025-06-21 09:58:44 +0545

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,120 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "JobHunt",
+    description:
+      "JobHunt is a web application that connects job seekers with employers. Users can create profiles, browse job listings, apply for positions, and manage applications—all from a centralized platform built using the MERN stack.",
+    subDescription: [
+      "Built a full-stack job portal using the MERN stack (MongoDB, Express.js, React, Node.js), enabling seamless interaction between job seekers and employers.",
+      "Implemented secure user authentication and role-based access control with JWT and bcrypt for data privacy.",
+      "Designed a modern, responsive frontend using Tailwind CSS to ensure smooth user experience across all devices.",
+      "Integrated features like job filtering, application tracking, resume uploads, and recruiter dashboards to enhance platform usability.",
+    ],
+    href: "https://myproject-43og.onrender.com",
+    logo: "",
+    image: "/assets/projects/jobhunt.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "NodeJs",
+        path: "/assets/logos/nodejs.svg",
+      },
+      {
+        id: 3,
+        name: "MongoDB",
+        path: "/assets/logos/mongodb.png",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "NodeChat",
+    description:
+      "NodeChat is a real-time messaging platform built with the MERN stack, allowing users to register, log in, send direct messages, and create group chats with seamless communication.",
+    subDescription: [
+      "Developed a real-time chat application using MongoDB, Express.js, React, and Node.js with Socket.io for instant message delivery.",
+      "Implemented secure user authentication using JWT and bcrypt, enabling protected login and session management.",
+      "Designed an intuitive and responsive user interface with Tailwind CSS for smooth user interactions across devices.",
+      "Enabled features like group chat creation, user search, typing indicators, and real-time message notifications for a dynamic chat experience.",
+    ],
+    href: "https://chatapp-2vbx.onrender.com",
+    logo: "",
+    image: "/assets/projects/chatapp2.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "NodeJs",
+        path: "/assets/logos/nodejs.svg",
+      },
+      {
+        id: 3,
+        name: "MongoDB",
+        path: "/assets/logos/mongodb.png",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+];
+export const mySocials = [
+  {
+    name: "WhatsApp",
+    href: "",
+    icon: "/assets/socials/whatsApp.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "www.linkedin.com/in/manoj-nath-sm29",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/manoz.nath?igsh=ZTRoaGhtZmtsM2du",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "B.Tech CSE Final Year Student",
+    job: "Full-Stack Web Developer (Project-Based Experience)",
+    date: "2022–2026",
+    contents: [
+      "Built and deployed multiple full-stack web applications using the MERN stack (MongoDB, Express.js, React, Node.js), focusing on real-world use cases like job portals and chat systems.",
+      "Developed a real-time chat app with group chat, authentication, and instant messaging using Socket.io, JWT, and Tailwind CSS.",
+      "Created a job portal platform with resume upload, job filtering, and user-role management to simulate a professional hiring workflow.",
+      "Applied version control using Git and GitHub, collaborating on project repositories and managing code deployments via Render and Vercel.",
+      "Explored backend development using Django for an online bookstore project, building foundational skills in Python web frameworks and database integration."
+    ],
+  },
+  {
+  title: "Aspiring Full-Stack Engineer",
+  job: "Future Software Developer | Passionate Problem Solver",
+  date: "2022–Present",
+  contents: [
+    "Actively building expertise in full-stack development using the MERN stack with a strong foundation in JavaScript, React, Node.js, and MongoDB.",
+    "Creating real-world projects like chat apps and job portals to simulate industry-grade software solutions and user experiences.",
+    "Consistently learning and experimenting with new technologies such as Next.js, Firebase, and cloud deployment platforms to stay ahead in the field.",
+    "Dedicated to mastering system design, algorithms, and scalable architecture to prepare for high-performance engineering roles.",
+    "Driven by the ambition to be among the top 1% of developers, continuously sharpening coding, problem-solving, and design thinking skills through personal projects and tech communities."
+  ],
+}
+];
---
+++ src/sections/About.jsx
@@ -0,0 +1,119 @@
+import { useRef } from "react";
+import Card from "../components/Card";
+import { Globe } from "../components/globe";
+import CopyEmailButton from "../components/CopyEmailButton";
+import { Frameworks } from "../components/FrameWorks";
+
+
+const About = () => {
+  const grid2Container = useRef();
+  return (
+    <section className="c-space section-spacing" id="about">
+      <h2 className="text-heading">About Me</h2>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-6 md:auto-rows-[18rem] mt-12">
+        {/* Grid 1 */}
+        <div className="flex items-end grid-default-color grid-1">
+          <img
+            src="assets/cdm.jpg"
+            className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
+          />
+          <div className="z-10">
+            <p className="headtext">Hi, I'm Manoj Nath</p>
+            <p className="subtext">
+              Over the last 1 year, I’ve honed my skills in frontend (HTML, CSS, JavaScript, React) and 
+              backend development (Node.js, Express, MongoDB) to build dynamic, responsive, and full-stack web applications.
+            </p>
+          </div>
+          <div className="absolute inset-x-0 pointer-evets-none -bottom-4 h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo" />
+        </div>
+        {/* Grid 2 */}
+        <div className="grid-default-color grid-2">
+          <div
+            ref={grid2Container}
+            className="flex items-center justify-center w-full h-full"
+          >
+            <p className="flex items-end text-5xl text-gray-500">
+              CODE IS CRAFT
+            </p>
+            <Card
+              style={{ rotate: "75deg", top: "30%", left: "20%" }}
+              text="REST APIs"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "-30deg", top: "60%", left: "45%" }}
+              text="React Hooks"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "90deg", bottom: "30%", left: "70%" }}
+              text="MVC Pattern"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "-45deg", top: "55%", left: "0%" }}
+              text="Middleware"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "20deg", top: "10%", left: "38%" }}
+              text="JWT"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "30deg", top: "70%", left: "70%" }}
+              image="assets/logos/mongodb.png"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "-45deg", top: "70%", left: "25%" }}
+              image="assets/logos/react.png"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "-45deg", top: "5%", left: "10%" }}
+              image="assets/logos/nodejs.png"
+              containerRef={grid2Container}
+            />
+          </div>
+        </div>
+        {/* Grid 3 */}
+        <div className="grid-black-color grid-3">
+          <div className="z-10 w-[50%]">
+            <p className="headtext">Time Zone</p>
+            <p className="subtext">
+              I'm based in Mars, and open to remote work worldwide
+            </p>
+          </div>
+          <figure className="absolute left-[30%] top-[10%]">
+            <Globe />
+          </figure>
+        </div>
+        {/* Grid 4 */}
+        <div className="grid-special-color grid-4">
+          <div className="flex flex-col items-center justify-center gap-4 size-full">
+            <p className="text-center headtext">
+              Do you want to start a project together?
+            </p>
+            <CopyEmailButton />
+          </div>
+        </div>
+        {/* Grid 5 */}
+        <div className="grid-default-color grid-5">
+          <div className="z-10 w-[50%]">
+            <p className="headText">Tech Stack</p>
+            <p className="subtext">
+              I specialize in building full-stack web applications using the MERN stack (MongoDB, Express.js, React, Node.js), 
+              with a strong focus on clean architecture, RESTful APIs, reusable components, and scalable backend systems.
+            </p>
+          </div>
+          <div className="absolute inset-y-0 md:inset-y-9 w-full h-full start-[50%] md:scale-125">
+            <Frameworks />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
